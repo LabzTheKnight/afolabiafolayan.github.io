460b566db9e95d039e3dc8e254dd42cdf4400f36
LabzTheKnight <aaafolayan@gmail.com>
2024-09-14 14:20:37 +0200

    intial commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React or not
+          Learn React or not react
         </a>
       </header>
     </div>
